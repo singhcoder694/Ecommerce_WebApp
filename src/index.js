e451b15b27e656ce62131d2287477517c7159155
singhcoder694 <vivekkr694@gmail.com>
2024-02-11 17:17:08 +0530

    Ecommerce Web

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import {createRoot} from "react-dom/client"
+import App from "./components/App"
+import Contact from "./components/Contact"
+import Product from "./components/Products"
+import Create from "./components/Createproduct"
+import Update from "./components/Update"
+import "bootstrap/dist/css/bootstrap.min.css"
+import 'bootstrap/dist/js/bootstrap.js'
+import './main/style.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+const router = createBrowserRouter([
+{
+    path: "/",
+    element: <App />,
+},
+{
+    path:"contact",
+    element: <Contact />
+},
+{
+    path:"products",
+    element:<Product />
+},
+{
+    path:"create",
+    element:<Create />
+},
+{
+    path:"update",
+    element:<Update />
+}
+]);
+const domNode=document.getElementById('root');
+const root=createRoot(domNode);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    <RouterProvider router={router} />
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
